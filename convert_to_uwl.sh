0b46966a02ef50c781554b857bd3ca25277b2428
Stefan Oslowski <sosl@users.noreply.github.com>
2018-11-13 23:59:16 +1100

    MED_CHAN -> MED_CHN, fdmode -> FA

--- convert_to_uwl.sh
+++ convert_to_uwl.sh
@@ -14,4 +14,4 @@ fi
 input=$1
 output=$2
 
-awk -F";" '{if (NR==1) print "# This was automatically converted. Needs DMs, search for XXX"; if (substr($1, 1, 1)=="#") print $0; else if (NF>1) {printf("%s ;%s ;", $1,$2); for (i=3; i < NF; i++) {sub(/^[ \t]+/, "", $i); key=substr($i, 1, 3); if (key=="d4_" || key=="CA_" || key=="tob" || key=="rcv" || key == "mod" || key == "cal" || key =="cfr" || key=="fdm" || key =="fda") printf("%s ;", $i)}; print "MED_PROC SRCH; MED_SUB 15; MED_BIT 8; MED_POL 4; MED_SMP 64; MED_CHAN 256; MED_DM XXX"} }' $input | sed 's/MULTI/UWL/g' | sed 's/BPSR/MEDUSA/g' > $output
+awk -F";" '{if (NR==1) print "# This was automatically converted. Needs DMs, search for XXX"; if (substr($1, 1, 1)=="#") print $0; else if (NF>1) {printf("%s ;%s ;", $1,$2); for (i=3; i < NF; i++) {sub(/^[ \t]+/, "", $i); key=substr($i, 1, 3); if (key=="d4_" || key=="CA_" || key=="tob" || key=="rcv" || key == "mod" || key == "cal" || key =="cfr" || key=="fdm" || key =="fda") printf("%s ;", $i)}; print "MED_PROC SRCH; MED_SUB 15; MED_BIT 8; MED_POL 4; MED_SMP 64; MED_CHN 256; MED_DM XXX"} }' $input | sed 's/MULTI/UWL/g' | sed 's/BPSR/MEDUSA/g' | sed 's/fdmode CPA/fdmode FA/> $output
